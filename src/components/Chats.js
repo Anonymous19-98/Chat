d66759ae7717e34410938b5dbe4c036bbc321b0f
Abdulaziz Almekhlafi <Abdulazuz716111@gmail.com>
2021-06-01 23:17:47 +0300

    there are an error in chatengine.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,87 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+import { Avatar, ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
 
+import { useAuth, userAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 
 const Chats = () => {
+
+    const history = useHistory();
+    const {user} = useAuth();  
+    const [loading, setLoading] = useState(true);
+    const  handleLogout = async () => {
+        await auth.signOut();
+        
+        history.push('/');
+    }
+
+    const getFile = async (url) => {
+        const response = await fetch(url);
+        const data = await response.blob();
+
+        return new File([data], "userPhoto.jpg", {type: 'image/jpeg' })
+    }
+    useEffect(() => {
+        if(!user){
+            history.push('/')
+            return;
+        }
+
+        axios.get('https://api.chatengine.io/users/me', {
+            headers: {
+                "project-id": "2d0aacc4-35b3-4f0a-8d59-b0f4426bb7e5",
+                "user-name": user.email,
+                "user-secret": user.uid,
+
+            }
+        })
+        .then(() => {
+            setLoading(false);
+        })
+        .catch(() =>{
+            let formdata = new FormData();
+            formdata.append('email', user.email);
+            formdata.append('username', user.displayName);
+            formdata.append('secret', user.uid);
+
+            getFile(user.photoURL)
+                .then((avatar) => {
+                    formdata.append('avatar', avatar, avatar.name);
+                    axios.post('https://api.chatengine.io/users',
+                        formdata,
+                        {headers:
+                            {"private-key": "9a2cbb24-5a56-45e0-8bc1-7a8da34674fd"}}
+                        )    
+                        .then(() => setLoading(false))
+                        .catch((error) => console.log(error))   
+                })
+        })
+    }, [user, history]);
+
+    if(!user || loading) return 'Loading ... ';
+
     return(
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Chat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine 
+                height ="calc(100vh - 66px)"
+                projectID="2d0aacc4-35b3-4f0a-8d59-b0f4426bb7e5"
+                userName={user.email}
+                userSecret={user.uid}
+            />
+        </div>
     )
 }
-
 
 export default Chats;
